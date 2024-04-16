127420a190a8b5b231ef7476f1618093c9ad5678
Alwurts <jota780@gmail.com>
2024-04-16 12:05:57 +0200

    Refactor CodeViewer component to add screen size options

--- src/components/CodeViewer.tsx
+++ src/components/CodeViewer.tsx
@@ -1,8 +1,26 @@
 import { useEffect, useState } from "react";
 import { editorManager } from "../lib/editor/EditorManager";
+import { Separator } from "./ui/separator";
+import { Button } from "./ui/button";
+import { Laptop, Smartphone, Tablet } from "lucide-react";
+import { cn } from "@/lib/utils";
 
 const CodeViewer = () => {
   const [srcDoc, setSrcDoc] = useState<string>("");
+
+  const [screenSize, setScreenSize] = useState<"mobile" | "tablet" | "desktop">(
+    "mobile",
+  );
+
+  const screenSizes = {
+    mobile: "w-[330px] h-[620px]",
+    tablet: "w-[768px] h-[1024px]",
+    desktop: "w-[1024px] h-[768px]",
+  };
+
+  const handleScreenSizeChange = (size: "mobile" | "tablet" | "desktop") => {
+    setScreenSize(size);
+  };
 
   useEffect(() => {
     const updateSrcDoc = (editorNotification: {
@@ -18,12 +36,44 @@ const CodeViewer = () => {
   }, []);
 
   return (
-    <div className="h-full w-full overflow-auto scrollbar scrollbar-thumb-neutral-700">
-      <iframe
-        title="Rendered Output"
-        className="m-auto h-[620px] w-[330px] rounded-xl border-2 border-black"
-        srcDoc={srcDoc}
-      />
+    <div className="flex h-full flex-col">
+      <div className="flex h-10 items-center justify-between px-6">
+        <h2 className="text-xs uppercase text-white">Display</h2>
+        <div className="flex space-x-1">
+          <Button
+            size="icon"
+            className="h-auto w-auto rounded-sm p-1 hover:bg-editor-accent"
+            onClick={() => handleScreenSizeChange("mobile")}
+          >
+            <Smartphone className="h-4 w-4 flex-shrink-0" />
+          </Button>
+          <Button
+            size="icon"
+            className="h-auto w-auto rounded-sm p-1 hover:bg-editor-accent"
+            onClick={() => handleScreenSizeChange("tablet")}
+          >
+            <Tablet className="h-4 w-4 flex-shrink-0" />
+          </Button>
+          <Button
+            size="icon"
+            className="h-auto w-auto rounded-sm p-1 hover:bg-editor-accent"
+            onClick={() => handleScreenSizeChange("desktop")}
+          >
+            <Laptop className="h-4 w-4 flex-shrink-0" />
+          </Button>
+        </div>
+      </div>
+      <Separator className="bg-editor-gray-light" />
+      <div className="h-full w-full flex-grow overflow-auto scrollbar scrollbar-thumb-neutral-700">
+        <iframe
+          title="Rendered Output"
+          className={cn(
+            "m-auto rounded-xl border-2 border-black bg-white",
+            screenSizes[screenSize],
+          )}
+          srcDoc={srcDoc}
+        />
+      </div>
     </div>
   );
 };
